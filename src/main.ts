81375a7e506af002d2fe951e701d34e44929579e
yuuta <yuuta_cookie@icloud.com>
2020-01-20 15:35:45 +0900

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000);
+  console.log('server started on http://localhost:4000/graphql');
+}
+bootstrap();
